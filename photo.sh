4cf937c34996dc9b322d80226a9fe328a5bbe5db
Adam Clarke <59942887+aclarke500@users.noreply.github.com>
2025-08-15 14:41:13 -0400

    Update photo.sh

--- photo.sh
+++ photo.sh
@@ -1,1 +1,17 @@
-fswebcam -r 640x480 ./test.jpg
+# fswebcam -r 640x480 ./test.jpg #!/usr/bin/env bash
+set -euo pipefail
+DEV=${1:-/dev/video0}
+OUT="test.jpg"
+
+if v4l2-ctl -d "$DEV" --list-formats-ext | grep -q MJPG; then
+  ffmpeg -hide_banner -loglevel error \
+    -f v4l2 -input_format mjpeg -framerate 30 -video_size 640x480 -i "$DEV" \
+    -frames:v 1 -y "$OUT"
+else
+  ffmpeg -hide_banner -loglevel error \
+    -f v4l2 -input_format yuyv422 -framerate 30 -video_size 640x480 -i "$DEV" \
+    -frames:v 1 -y "$OUT"
+fi
+
+sync
+[ -s "$OUT" ]

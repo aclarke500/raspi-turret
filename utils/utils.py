fcc8257cb09b5e9d870170d42dbfbc058df1f3b4
Adam <aclarke500@gmail.com>
2025-08-16 18:27:00 -0400

    flipped sign to see if that fixes things

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,4 @@
 def x_offset_to_degrees(x_offset):
     # fov is 55 degrees, since x is signed 
     # we multiply by 27.5 and that tells us our offset
-    return x_offset*27.5
+    return -1*x_offset*27.5

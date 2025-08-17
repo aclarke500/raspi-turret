e716af38eb5a7f640762e43da9794f1f4cbe9ebb
Adam Clarke <59942887+aclarke500@users.noreply.github.com>
2025-08-17 00:30:05 -0400

    added traverse to main

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ T = Turret()  # Create an instance
 T.setup()
 
 while True:
-    x_offset_degrees, y_offset_degrees = T.patrol()
-    if x_offset_degrees is not None:
-        T.snap_to_target(x_offset_degrees, y_offset_degrees)
+    # x_offset_degrees, y_offset_degrees = T.patrol()
+    # if x_offset_degrees is not None:
+    #     T.snap_to_target(x_offset_degrees, y_offset_degrees)
+    time.sleep(2)
+    T.traverse()
     

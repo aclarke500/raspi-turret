4246aa328f1546d09101e654716b112eb15f7d1c
Adam Clarke <59942887+aclarke500@users.noreply.github.com>
2025-08-16 20:30:50 -0400

    Create camera.py

---
+++ utils/camera.py
@@ -0,0 +1,19 @@
+import cv2
+import threading
+
+cap = cv2.VideoCapture(0)
+current_frame = None
+
+def update_frame():
+    global current_frame
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            current_frame = frame
+
+# start grabbing in background
+t = threading.Thread(target=update_frame, daemon=True)
+t.start()
+
+def get_frame():
+    return current_frame

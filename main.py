e3b050b55a39de66eae6b32cd58e7657f3fd4fd2
Adam Clarke <59942887+aclarke500@users.noreply.github.com>
2025-08-17 00:41:12 -0400

    added cleanup to main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from Turret import Turret
+import signal, sys
+
+
 T = Turret()  # Create an instance
 T.setup()
+
+def handle_exit(sig, frame):
+    turret.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_exit)
+signal.signal(signal.SIGTERM, handle_exit)
 
 while True:
     # x_offset_degrees, y_offset_degrees = T.patrol()

19e43434ffdf027632b91bb7eb36992c279b0921
Adam <aclarke500@gmail.com>
2025-08-16 21:55:08 -0400

    updated offset logic

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ T = Turret()  # Create an instance
 T.setup()
 
 while True:
-    offset = T.patrol()
+    x_offset_degrees, y_offset_degrees = T.patrol()
+    if x_offset_degrees is not None:
+        T.snap_to_target(x_offset_degrees, y_offset_degrees)
     
